33a91b320597d3b6ceec4701bd483e9133abf72b
qazsato <qazsato@gmail.com>
2020-09-17 11:07:36 +0900

    fix prettier lint

--- client/config/index.js
+++ client/config/index.js
@@ -2,9 +2,6 @@ import config from './default.json'
 import development from './development.json'
 import production from './production.json'
 
-const c =
-  process.env.NODE_ENV === 'production'
-    ? { ...config, ...production }
-    : { ...config, ...development }
+const c = process.env.NODE_ENV === 'production' ? { ...config, ...production } : { ...config, ...development }
 
 export default c
