8c5388978a3d62541a0f636273999799edf1b55c
qazsato <qazsato@gmail.com>
2020-09-18 21:13:27 +0900

    円グラフ追加

--- client/components/DataDrawer.vue
+++ client/components/DataDrawer.vue
@@ -9,18 +9,20 @@
     <el-tabs v-model="activeTab" :stretch="true">
       <el-tab-pane label="表" name="table">
         <el-table :data="data" :default-sort="{ prop: 'count', order: 'descending' }">
-          <el-table-column prop="key" label="名前" sortable></el-table-column>
+          <el-table-column prop="key" label="ポリゴン" sortable></el-table-column>
           <el-table-column prop="count" label="件数" width="100" sortable></el-table-column>
         </el-table>
       </el-tab-pane>
       <el-tab-pane label="グラフ" name="graph">
-        <canvas ref="chart"></canvas>
+        <canvas ref="chart" width="100%" height="100%"></canvas>
       </el-tab-pane>
     </el-tabs>
   </el-drawer>
 </template>
 
 <script>
+import Chart from 'chart.js'
+
 export default {
   props: {
     title: {
@@ -43,19 +45,36 @@ export default {
     return {
       activeTab: 'table',
       innerVisible: this.visible,
+      chart: null,
     }
   },
 
   watch: {
     visible(val) {
-      console.log('visible', val)
       this.innerVisible = val
+      this.$nextTick(() => {
+        this.createChart()
+      })
     },
   },
 
   methods: {
     onBeforeClose() {
       this.$emit('close')
+    },
+
+    createChart() {
+      this.chart = new Chart(this.$refs.chart, {
+        type: 'pie',
+        data: {
+          labels: this.data.map((d) => d.key),
+          datasets: [
+            {
+              data: this.data.map((d) => d.count),
+            },
+          ],
+        },
+      })
     },
   },
 }
