1dc962bc10862ca5eb85435b9de09ac023d228f3
qazsato <qazsato@gmail.com>
2020-09-18 21:36:21 +0900

    不要なコメント削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,11 @@
 // eslint-disable-next-line nuxt/no-cjs-in-config
 module.exports = {
   srcDir: 'client/',
-  /*
-   ** Nuxt rendering mode
-   ** See https://nuxtjs.org/api/configuration-mode
-   */
+
   mode: 'universal',
-  /*
-   ** Nuxt target
-   ** See https://nuxtjs.org/api/configuration-target
-   */
+
   target: 'server',
-  /*
-   ** Headers of the page
-   ** See https://nuxtjs.org/api/configuration-head
-   */
+
   head: {
     title: 'Geo Tool',
     meta: [
@@ -28,30 +19,19 @@ module.exports = {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  /*
-   ** Global CSS
-   */
+
   css: ['element-ui/lib/theme-chalk/index.css'],
+
   styleResources: {
     scss: ['@/assets/styles/core.scss'],
   },
-  /*
-   ** Plugins to load before mounting the App
-   ** https://nuxtjs.org/guide/plugins
-   */
+
   plugins: ['@/plugins/element-ui'],
-  /*
-   ** Auto import components
-   ** See https://nuxtjs.org/api/configuration-components
-   */
+
   components: true,
-  /*
-   ** Nuxt.js dev-modules
-   */
+
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
     '@nuxtjs/style-resources',
     [
@@ -61,22 +41,11 @@ module.exports = {
       },
     ],
   ],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-  ],
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
+
+  modules: ['@nuxtjs/axios'],
+
   axios: {},
-  /*
-   ** Build configuration
-   ** See https://nuxtjs.org/api/configuration-build/
-   */
+
   build: {
     transpile: [/^element-ui/],
   },
