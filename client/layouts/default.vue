3a627340df57d35204dc620609d47b7f67174db8
qazsato <qazsato@gmail.com>
2020-09-18 19:20:55 +0900

    vh用のカスタムプロパティ追加

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -4,6 +4,22 @@
   </div>
 </template>
 
+<script>
+export default {
+  mounted() {
+    this.setStyleProperty()
+    window.addEventListener('resize', this.setStyleProperty)
+  },
+
+  methods: {
+    setStyleProperty() {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    },
+  },
+}
+</script>
+
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
@@ -22,34 +38,5 @@ html {
 *::after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
