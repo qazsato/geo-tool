97fd2d019a9ac36978557c9ecc733261af50a126
qazsato <qazsato@gmail.com>
2020-09-20 13:42:33 +0900

    テーマカラー設定

--- client/plugins/element-ui.js
+++ client/plugins/element-ui.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Element from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale/lang/ja'
+import '@/assets/styles/element-variables.scss'
 
 Vue.use(Element, { locale })
