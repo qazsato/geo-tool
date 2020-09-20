9d9d7197dc5d88e8a3e31ede05dfaf1e1ee55e6d
qazsato <qazsato@gmail.com>
2020-09-21 00:43:24 +0900

    タイトルのスタイル修正

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header">
-    <h1 v-if="title" class="title">{{ title }}</h1>
-    <Logo v-else href="/" />
+    <Logo href="/" />
+    <h1 class="title">{{ title }}</h1>
     <div class="spacer"></div>
     <el-button class="import-button" icon="el-icon-place" @click="onClickImport">データ読み込み</el-button>
     <GithubCorner fill="#bbb" />
@@ -31,6 +31,16 @@ export default {
   display: flex;
   align-items: center;
   padding: 0 60px 0 20px;
+}
+
+.title {
+  margin-left: 10px;
+  font-size: 18px;
+  font-weight: normal;
+  color: #303133;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 
 .spacer {
