3405d2346ab3fa28167bdbed1470728ef0ce2f20
qazsato <qazsato@gmail.com>
2020-09-21 23:18:41 +0900

    Add satellite theme

--- client/constants/view-map-state.js
+++ client/constants/view-map-state.js
@@ -2,4 +2,4 @@ import Enum from 'enum'
 
 export const analysisType = new Enum({ address: 1, mesh: 2, heatmap: 3, cluster: 4 })
 
-export const mapTheme = new Enum({ standard: 1, silver: 2, retro: 3, night: 4, dark: 5, aubergine: 6 })
+export const mapTheme = new Enum({ standard: 1, silver: 2, retro: 3, night: 4, dark: 5, aubergine: 6, satellite: 7 })
