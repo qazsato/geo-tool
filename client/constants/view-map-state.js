61a4edc019010385d3f48fe44a5498cde254a995
qazsato <qazsato@gmail.com>
2020-09-21 19:04:18 +0900

    地図テーマ定数追加

--- client/constants/view-map-state.js
+++ client/constants/view-map-state.js
@@ -1,3 +1,5 @@
 import Enum from 'enum'
 
 export const analysisType = new Enum({ address: 1, mesh: 2, heatmap: 3, cluster: 4 })
+
+export const mapTheme = new Enum({ standard: 1, silver: 2, retro: 3, night: 4, dark: 5, aubergine: 6 })
