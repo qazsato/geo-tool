7adde7b0f3786042fd0d7314bf7321919e924f16
qazsato <qazsato@gmail.com>
2020-10-02 00:41:37 +0900

    add lang attrs

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ module.exports = {
 
   head: {
     titleTemplate: '%s - Geo Tool',
+    htmlAttrs: { lang: 'ja' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
