14e6ba08891caa90242c019bef0f41462c6a246b
qazsato <qazsato@gmail.com>
2020-10-02 00:39:49 +0900

    add google adsense

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,15 @@ module.exports = {
     ],
   ],
 
-  modules: ['@nuxtjs/axios'],
+  modules: [
+    '@nuxtjs/axios',
+    [
+      '@nuxtjs/google-adsense',
+      {
+        id: 'ca-pub-3757647132963072',
+      },
+    ],
+  ],
 
   axios: {},
 
