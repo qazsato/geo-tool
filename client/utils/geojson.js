c8e43f828cfbe0134013ef90980e746ba82540ef
qazsato <qazsato@gmail.com>
2020-12-14 00:32:40 +0900

    GeometryCollectionの判定追加

--- client/utils/geojson.js
+++ client/utils/geojson.js
@@ -7,22 +7,30 @@ import _ from 'lodash'
 export const toLocations = (geojson) => {
   let coords = []
   if (geojson.type === 'Feature') {
-    if (geojson.geometry.type === 'Polygon') {
-      coords = _.flattenDepth(geojson.geometry.coordinates, 1)
-    } else if (geojson.geometry.type === 'MultiPolygon') {
-      coords = _.flattenDepth(geojson.geometry.coordinates, 2)
+    if (geojson.geometry.type === 'GeometryCollection') {
+      geojson.geometry.geometries.forEach((geometry) => {
+        coords = coords.concat(getGeometryCoords(geometry))
+      })
+    } else {
+      coords = getGeometryCoords(geojson.geometry)
     }
   } else if (geojson.type === 'FeatureCollection') {
     const features = geojson.features
     features.forEach((f) => {
-      if (f.geometry.type === 'Polygon') {
-        coords = coords.concat(_.flattenDepth(f.geometry.coordinates, 1))
-      } else if (f.geometry.type === 'MultiPolygon') {
-        coords = coords.concat(_.flattenDepth(f.geometry.coordinates, 2))
-      }
+      coords = coords.concat(getGeometryCoords(f.geometry))
     })
   }
   return coords.map((c) => {
     return { lat: c[1], lng: c[0] }
   })
+}
+
+function getGeometryCoords(geometry) {
+  let coords = []
+  if (geometry.type === 'Polygon') {
+    coords = _.flattenDepth(geometry.coordinates, 1)
+  } else if (geometry.type === 'MultiPolygon') {
+    coords = _.flattenDepth(geometry.coordinates, 2)
+  }
+  return coords
 }
