9f375d3941f3dec067c4b4ba5b8787c2d79e7227
qazsato <qazsato@gmail.com>
2021-02-09 10:21:46 +0900

    limit 追加

--- client/utils/map-data.js
+++ client/utils/map-data.js
@@ -86,7 +86,10 @@ function fetchAddressShape(allCodes) {
   const codes = _.chunk(allCodes, MAX_CODE_COUNT)
   return new Promise((resolve, reject) => {
     const promises = codes.map((code) => {
-      const api = new GeoApi('/addresses/shapes', { codes: code.toString() })
+      const api = new GeoApi('/addresses/shapes', {
+        codes: code.toString(),
+        limit: MAX_CODE_COUNT,
+      })
       return api.get()
     })
     axios
