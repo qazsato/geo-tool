2cab2e55653c20063c80bf6dc3dcad7308faaf76
qazsato <qazsato@gmail.com>
2021-03-21 22:13:49 +0900

    住所レベル3,4名称変更

--- client/constants/address.js
+++ client/constants/address.js
@@ -5,6 +5,6 @@ export const addressLevel = new Enum({ level1: 1, level2: 2, level3: 3, level4: 
 export const addressLevelName = new Enum({
   level1: '都道府県',
   level2: '市区町村',
-  level3: '大字・町',
-  level4: '丁目・字等',
+  level3: '大字・町名',
+  level4: '字・丁目',
 })
