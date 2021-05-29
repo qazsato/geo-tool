fb8d86f511b33f9b4da38c936848dad01a31da8f
qazsato <qazsato@gmail.com>
2021-05-29 17:58:55 +0900

    解析上限値の変更

--- client/constants/address.js
+++ client/constants/address.js
@@ -1,11 +1,11 @@
 import Enum from 'enum'
 
 // 解析上限
-export const ANALYTICS_LIMIT = 10000
+export const ANALYTICS_LIMIT = 50000
 // 解析 API の一度の呼び出しでの最大指定数
 export const ANALYTICS_LIMIT_PER_REQUEST = 10000
 // 住所ポリゴン表示上限
-export const POLYGON_LIMIT = 1000
+export const POLYGON_LIMIT = 5000
 // ポリゴン API の一度の呼び出しでの最大指定数
 export const POLYGON_LIMIT_PER_REQUEST = 100
 
