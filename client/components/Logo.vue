92f4850fd6d56a2198f4b6c351f4934ad5ad236a
qazsato <qazsato@gmail.com>
2023-01-05 09:03:02 +0900

    ロゴ変更

--- client/components/Logo.vue
+++ client/components/Logo.vue
@@ -1,7 +1,6 @@
 <template>
   <nuxt-link class="logo" :to="url">
-    <span>Geo Tool</span>
-    <img src="~/assets/images/planet.svg" />
+    <img src="~/assets/images/logo.svg" />
   </nuxt-link>
 </template>
 
@@ -18,34 +17,12 @@ export default {
 
 <style lang="scss" scoped>
 .logo {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-width: 120px;
-  margin: 10px 0;
-  padding: 5px 0;
-  border-radius: 3px;
-  background: #eee;
-  color: #3b4151;
-  line-height: 19px;
-  text-decoration: none;
-  font-weight: bold;
-
-  @include xs() {
-    min-width: 36px;
-    padding: 8px 0;
-  }
-
-  > span {
-    margin-right: 6px;
-
-    @include xs() {
-      display: none;
-    }
-  }
+  height: 30px;
+  width: 110px;
 
   > img {
-    width: 20px;
+    height: 100%;
+    width: 100%;
   }
 }
 </style>
